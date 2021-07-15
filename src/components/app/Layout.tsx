6e5d5c042983e92c80e21e5a2fb357c137434cf0
Petr Cibulka <cibulka.me@gmail.com>
2021-07-15 17:05:11 +0200

    favicons

--- src/components/app/Layout.tsx
+++ src/components/app/Layout.tsx
@@ -1,11 +1,12 @@
-import React, { FC, ReactChild } from "react";
+import React, { FC, ReactChild } from 'react';
+import Head from 'next/head';
 
-import useGlobalStyles from "src/hooks/useGlobalStyles";
-import useLocalStorage from "src/hooks/useLocalStorage";
+import useGlobalStyles from 'src/hooks/useGlobalStyles';
+import useLocalStorage from 'src/hooks/useLocalStorage';
 
-import Nav from "./Nav";
+import Nav from './Nav';
 
-import "tailwindcss/tailwind.css";
+import 'tailwindcss/tailwind.css';
 
 const Layout: FC<{
   children: ReactChild;
@@ -18,6 +19,15 @@ const Layout: FC<{
 
   return (
     <>
+      <Head>
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="theme-color" content="#ffffff" />
+      </Head>
       {props.children}
       <Nav />
     </>
